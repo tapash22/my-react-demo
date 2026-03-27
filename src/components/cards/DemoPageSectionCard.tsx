949f087e0a198a64ff9e7235be13e450c5715387
tapash22 <tapasp263@gmail.com>
2026-03-27 11:15:55 +0600

    update

--- src/components/cards/DemoPageSectionCard.tsx
+++ src/components/cards/DemoPageSectionCard.tsx
@@ -18,7 +18,7 @@ export function DemoPageSectionCard({
     <div
       className={`w-full h-auto flex justify-between items-center rounded-xl space-y-1 ${haveBorder ? "ring-1 ring-(--input-border) p-4 hover:ring-gray-400  cursor-pointer" : "px-2 py-1"}`}
     >
-      <div className="flex flex-col justify-start space-y-1">
+      <div className="flex flex-col justify-center sm:justify-center md:justify-start items-center sm:items-center md:items-start space-y-1 w-full">
         <h2 className="text-lg font-normal tracking-normal text-(--foreground)">
           {title ? title : item?.title}
         </h2>
@@ -27,7 +27,7 @@ export function DemoPageSectionCard({
           {subtitle ? subtitle : item?.subtitle}
         </p>
       </div>
-      <div className="w-1/3 h-auto">{children && children}</div>
+      {children && <div className="w-1/3 h-auto">{children}</div>}
     </div>
   );
 }
