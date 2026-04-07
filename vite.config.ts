8898780af0dae2a16027d416f19b7a18a8abf7ee
tapash22 <tapasp263@gmail.com>
2026-04-08 00:20:51 +0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  //handle local server port
-  // server: {
-  //   port: 5000,
-  //   strictPort: true,
-  //   open: true, // Optional: opens the browser automatically
-  // },
+  base: "./", // Add this line to ensure paths are relative
+  // handle local server port
+  server: {
+    port: 5173,
+    strictPort: true,
+    open: true, // Optional: opens the browser automatically
+  },
   preview: {
     port: 5000,
     strictPort: true,
